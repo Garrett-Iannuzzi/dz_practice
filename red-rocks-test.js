5d0823fb26fffc3cde6a6efe898a86419b32f4db
Garrett Iannuzzi <garrettiannuzzi@gmail.com>
2020-04-23 09:45:30 -0600

    Add testing for max capacity method

--- red-rocks-test.js
+++ red-rocks-test.js
@@ -3,32 +3,44 @@ const RedRocks = require('./red-rocks.js');
 
 describe('Red Rocks', function () {
 
-  it('should have a name of a show', function () {
+  it('should have a show with a name', function () {
     const redRocks = new RedRocks('Some Hip-Hop shit');
 
-    assert.equal(redRocks.showType, 'Some Hip-Hop shit');
+    assert.equal(redRocks.show, 'Some Hip-Hop shit');
   });
 
   it('should be able to have a list of upcoming shows', () => {
-    const redRocks = new RedRocks('Some Hip-Hop shit');
+    const redRocks = new RedRocks('Ice T');
 
     assert.deepEqual(redRocks.upcomingShows, [])
   });
 
   it('should have a capacity', () => {
-    const redRocks = new RedRocks('Some Hip-Hop shit');
+    const redRocks = new RedRocks('ACDC');
 
     assert.equal(redRocks.capacity, 9525)
   });
 
+  it('should be able to have a max capacity', () => {
+    const redRocks = new RedRocks('Biggy');
+    const currentCapacity = 8175;
+    const secondCurrentCapacity = 10181;
+
+    redRocks.atCapacity(currentCapacity);
+    redRocks.atCapacity(secondCurrentCapacity);
+
+    assert.equal(redRocks.atCapacity(currentCapacity), 'Have fun!');
+    assert.equal(redRocks.atCapacity(secondCurrentCapacity), 'Sorry, full...');
+  });
+
   it('should have the current weather', () => {
-    const redRocks = new RedRocks('Some Hip-Hop shit', { temp: 75, status: 'mostly sunny' });
+    const redRocks = new RedRocks('2 Pac', { temp: 75, status: 'mostly sunny' });
 
     assert.deepEqual(redRocks.todaysWeather, { temp: 75, status: 'mostly sunny' });
   });
 
   it('should be able to update the weather', () => {
-    const redRocks = new RedRocks('Some Hip-Hop shit', { temp: 75, status: 'mostly sunny' });
+    const redRocks = new RedRocks('Dr. Dre', { temp: 75, status: 'mostly sunny' });
     const weatherUpdate = { temp: 81, status: 'possible rain' };
 
     redRocks.updateWeather(weatherUpdate);
@@ -37,7 +49,7 @@ describe('Red Rocks', function () {
   });
 
   it('should be able to add some upcoming shows', () => {
-    const redRocks = new RedRocks('Some Hip-Hop shit', { temp: 75, status: 'mostly sunny' });
+    const redRocks = new RedRocks('Snoop', { temp: 75, status: 'mostly sunny' });
     const showToAdd = 'Pretty Lights';
 
     redRocks.addShow(showToAdd);
--- red-rocks.js
+++ red-rocks.js
@@ -1,10 +1,17 @@
 class RedRocks {
-  constructor(show, weatherDetails) {
-    this.showType = show;
+  constructor(showName, weatherDetails) {
+    this.show = showName;
     this.upcomingShows = [];
     this.capacity = 9525;
     this.todaysWeather = weatherDetails;
 
+  }
+
+  atCapacity(currentCapacity) {
+    if(this.capacity > currentCapacity) {
+      return 'Have fun!'
+    }
+    return 'Sorry, full...'
   }
 
   updateWeather(update) {
